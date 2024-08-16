3a51f5ac54d13e49f5418819f0182e40274d3cb7
ahoang2502 <athoang252@gmail.com>
2024-08-16 20:13:18 +1000

    feat: navbar

--- src/app/_components/Logo.tsx
+++ src/app/_components/Logo.tsx
@@ -1,15 +1,18 @@
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 export const Logo = () => {
   return (
     <div className="size-8 relative shrink-0">
-      <Image
-        src="/logo.svg"
-        fill
-        alt="logo"
-        className="shrink-0 hover:opacity-75 transition"
-      />
+      <Link href="/" className="flex items-center justify-center">
+        <Image
+          src="/logo.svg"
+          fill
+          alt="logo"
+          className="shrink-0 hover:opacity-75 transition size-8"
+        />
+      </Link>
     </div>
   );
 };
--- src/app/_components/Navbar.tsx
+++ src/app/_components/Navbar.tsx
@@ -7,18 +7,23 @@ import { Button } from "@/components/ui/button";
 
 export const Navbar = () => {
   return (
-    <div className="h-[50px] w-full bg-pink-300 flex items-center justify-between px-6">
-      <Logo />
-
-      <div className="max-w-2xl xs:w-[480px] sm:w-[500px] md:w-[680px] lg:w-[700px] bg-green-500 flex justify-between">
-        {navbarItems.map((item) => (
-          <Link href={item.href} key={item.value} className="">
-            {item.value}
-          </Link>
-        ))}
+    <div className="h-[50px] w-full bg-pink-300 flex items-center justify-between px-6 space-x-6">
+      <div className="flex justify-center items-center">
+        <Logo />
+        <p className="">Name</p>
       </div>
 
-      <Button>Login</Button>
+      <div className="max-w-7xl w-full flex items-center justify-end space-x-4">
+        <div className="min-w-[300px] flex items-center justify-between">
+          {navbarItems.map((item) => (
+            <Link href={item.href} key={item.value} className="text-sm ">
+              {item.value}
+            </Link>
+          ))}
+        </div>
+
+        <Button>Login</Button>
+      </div>
     </div>
   );
 };
