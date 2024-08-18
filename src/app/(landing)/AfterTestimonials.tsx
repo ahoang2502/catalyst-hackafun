1e657c93d3105f91b305bd51cafc748d94dc4f6d
ahoang2502 <athoang252@gmail.com>
2024-08-18 12:24:14 +1000

    style: add more images

--- src/app/(landing)/AfterTestimonials.tsx
+++ src/app/(landing)/AfterTestimonials.tsx
@@ -1,5 +1,24 @@
+import Image from "next/image";
 import React from "react";
 
 export const AfterTestimonials = () => {
-  return <div className="mt-8">AfterTestimonials</div>;
+  return (
+    <div className="mt-8 flex justify-center items-center">
+      <div className="relative size-40">
+        <Image alt="image" src="/img-1.png" fill className="object-contain"/>
+      </div>
+      <div className="relative size-40">
+        <Image alt="image" src="/img-2.png" fill className="object-contain"/>
+      </div>
+      <div className="relative size-40">
+        <Image alt="image" src="/img-3.png" fill className="object-contain"/>
+      </div>
+      <div className="relative size-40">
+        <Image alt="image" src="/img-4.png" fill className="object-contain"/>
+      </div>
+      <div className="relative size-40">
+        <Image alt="image" src="/img-5.png" fill className="object-contain"/>
+      </div>
+    </div>
+  );
 };
