6366437e83401a24efc249b05da610a1d00ad4a1
ahoang2502 <athoang252@gmail.com>
2024-08-18 14:49:22 +1000

    build: add key

--- src/app/(platform)/ourteam/page.tsx
+++ src/app/(platform)/ourteam/page.tsx
@@ -29,7 +29,7 @@ const OurTeamPage = () => {
     <div className="h-full w-full flex items-center justify-center -mt-16">
       <div className="border-blue-500 border-2 rounded-3xl p-8 px-16 w-[400px] space-y-3">
         {teamMembers.map((mem) => (
-          <div className="flex items-center justify-start w-full space-x-3">
+          <div className="flex items-center justify-start w-full space-x-3" key={mem.github}>
             <div className="flex space-x-3 items-center">
               <div className="relative size-16">
                 <Image src="/img.jpg" alt={mem.name} fill />
