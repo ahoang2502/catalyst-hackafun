d68d94e38ca35208c2072af8fbcebdacd43db0d8
ahoang2502 <athoang252@gmail.com>
2024-08-18 16:51:15 +1000

    fix: change testimonials

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -10,12 +10,12 @@ export const navbarItems = [
 export const testimonials = [
   {
     name: "John Smith",
-    text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laudantiu mollitia aut laboriosam accusamus? Alias, quam aut labore hic magnam repellendus reprehenderit id cum dolor non, mollitia ullam.",
+    text: "I recently discovered FindMine, and it has quickly become my go-to app for exploring new music venues! As someone who values accessibility, I was thrilled to find a tool that not only helps me locate live music events but also provides detailed reviews on venue accessibility. It's incredibly helpful to read first-hand accounts from other users about the ease of getting around and the accommodations available. The user reviews are honest and informative, making it so much easier to plan outings without any surprises. Whether you’re a music lover with specific accessibility needs or just someone who appreciates a smooth experience, FindMind is a game-changer. It’s great to see a tool that genuinely cares about making live music accessible to everyone!",
     image: "/user-woman.webp",
   },
   {
     name: "Sarah Smith",
-    text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laudantiu mollitia aut laboriosam accusamus? Alias, quam aut labore hic magnam repellendus reprehenderit id cum dolor non, mollitia ullam.",
+    text: "FindMine has revolutionized how I approach attending music events! As someone with mobility challenges, finding accessible venues has always been a bit of a hassle, but this app has made the process seamless. The reviews from other users are incredibly detailed and provide real insights into the accessibility features of various venues. I love that I can get a sense of what to expect before I even set foot in a place. It’s reassuring to know that I’m choosing venues that will accommodate my needs and enhance my experience. FindMind not only helps me discover new music but also ensures that I can enjoy it without any accessibility concerns. It’s a fantastic resource for anyone who wants to enjoy live music without the stress of accessibility issues.",
     image: "/user-male.webp",
   },
 ];
